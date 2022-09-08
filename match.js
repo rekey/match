738010e75328ec76532925edd2f5fccf56fcb5fd
Rekey <rekey@me.com>
2022-09-08 23:22:09 +0800

    save

--- match.js
+++ match.js
@@ -1,13 +1,36 @@
-const {curly} = require('node-libcurl');
+const {spawn} = require('child_process');
 
 function match(hostname, ip, port, https = false) {
-    const url = `${https ? 'https' : 'http'}://${hostname}:${port}`;
-    return curly(
-        url,
-        {
-            resolve : `${hostname}:${port}:${ip}`
-        }
-    );
+    return new Promise((resolve, reject) => {
+        const url = `${https ? 'https' : 'http'}://${hostname}:${port}`;
+        let child = spawn('curl', [
+            url,
+            '--resolve',
+            `${hostname}:${port}:${ip}`,
+        ]);
+        let data = Buffer.from('');
+        child.stdout.on('data', (chunk) => {
+            data = Buffer.concat([data, chunk]);
+        });
+        child.once('exit', (code) => {
+            process.nextTick(() => {
+                child.stdout.removeAllListeners();
+                child.kill(9);
+            });
+            if (code === 0) {
+                resolve(data);
+                return;
+            }
+            reject(code);
+        });
+        // child.once('error', (err) => {
+        //     reject(err);
+        // });
+    });
 }
+
+match('one.one.one.one', '1.1.1.1', 443, true)
+    .then(console.log)
+    .catch(console.error);
 
 module.exports = match;
